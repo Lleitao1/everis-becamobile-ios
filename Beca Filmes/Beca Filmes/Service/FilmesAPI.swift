7e4a5b6421f90998c4bb734c040feb369d6e380c
Lleitao1 <lucas.leitao25@gmail.com>
2021-01-12 15:29:01 -0300

    organizar pastas e iniciar separacao de responsabilidades

---
+++ Beca Filmes/Beca Filmes/Service/FilmesAPI.swift
@@ -0,0 +1,35 @@
+//
+//  FilmesAPI.swift
+//  Beca Filmes
+//
+//  Created by Lucas Abdel Leitao on 07/01/21.
+//  Copyright © 2021 Lucas Abdel Leitao. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class FilmesAPI: NSObject {
+    
+    // MARK: - Get
+    func recebeTendenciasFilmes(completion: @escaping ([Result]) -> Void) {
+        Alamofire.request("https://api.themoviedb.org/3/trending/all/week?api_key=2536c8a939fafc0cfd57dcda9d9ce039&language=pt-BR", method: .get).responseJSON { (resposta) in
+            switch resposta.result{
+            case .success:
+                
+                guard let jsonData = resposta.data else {return}
+                guard let welcome = try? JSONDecoder().decode(Filme.self, from: jsonData) else {return}
+                
+                let filmes = welcome.results
+                
+                completion(filmes)
+                
+                break
+            case .failure:
+                print(resposta.error!)
+                break
+            }
+        }
+    }
+    
+}
